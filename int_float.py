596d13670925bddd8b3477b55564ad50ac50ceea
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-11 18:46:52 +0530

    Add files via upload

---
+++ int_float.py
@@ -0,0 +1,30 @@
+# """
+# Name: Karthik
+# Date: 11th May 2022
+# Day: Wednesday
+# Time: 6:30-7:00PM
+# File Description:  INT/FLOAT/STRING
+# """
+'''''INT + FLOAT'''''''''''''''
+# a=2;b=4.5
+# print(type(a));print(type(b))
+# print(a+b)
+''''' STRING+INT/FLOAT'''''''''''''''
+# r='karthik' ; m=12 ; v=4.3
+# print(type(r)) #  str
+# print(type(m)) #  int
+# print(r+m) # TypeError
+# print(r+v) # TypeError
+
+# # Python ---> 5 times
+# print("python "*5)
+
+# # String *float
+# # print('hi' * 2.4)
+
+# # String+String
+# a="hello";b="hi"
+# print(a+b)
+
+
+# # print('hello' * 'hi') 
